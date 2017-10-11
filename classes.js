8e42ce486ba6a69d59d7358b4c3d41b5c6f77e4b
Dino Mandic <dinomandic@hotmail.com>
2017-10-11 14:23:17 -0500

    Solved first two problems

--- classes.js
+++ classes.js
@@ -22,7 +22,18 @@ Each employee can:
 call your class Employee and receive all the data in the constructor in the order listed
 */
 
+class Employee {
+    constructor(first_name, last_name, email, age) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.age = age;
+    }
 
+    makeWidget() {
+        return `${this.first_name} ${this.last_name} Widget`
+    }
+}
 
 /*
 
@@ -41,6 +52,22 @@ call your class Manager
 
 */
 
+class Manager extends Employee {
+    
+    constructor(first_name, last_name, email, age, reports) {
+        super(first_name, last_name, email, age)
+        this.reports = [];
+    }
+
+    hire(employee) {
+        this.reports.push(employee);
+    }
+
+    fire(index) {
+        this.reports.splice(index, 1);
+    }
+
+}
 
 
 
